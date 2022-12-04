9cddbb4d0c862c242a9d5bf3bd925064e9053845
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-04 15:17:42 +0100

    cezar v2

--- kod_cezara.py
+++ kod_cezara.py
@@ -1,8 +1,44 @@
-
-text = str(input("Wpisz text do zaszyfrowania: "))
+# SZYFR CEZARA
 
 dlugosc = len(text)
-
+wynik = []
 alfabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 
+def szyfruj():
+    text = str(input("Wpisz text do zaszyfrowania: "))
+
+    for i in text:
+        if str(i)  == " ":
+            wynik.append(" ")
+        else:
+            index = alfabet.index(i)
+            index = int(index) + 3
+            if index > 25:
+                index = index - 26
+            wynik.append(alfabet[index])
+
+def odszyfruj():
+    text = str(input("Wpisz text do odszyfrowania: "))
+    for i in text:
+        if str(i)  == " ":
+            wynik.append(" ")
+        else:
+            index = alfabet.index(i)
+            index = int(index) + 3
+            if index > 25:
+                index = index - 26
+            wynik.append(alfabet[index])
+
+
+print("Szyfruj -> 1\nOdszyfruj -> 2")
+ask = input("")
+
+if ask.isdigit():
+    if int(ask) == 1:
+        szyfruj()
+    elif int(ask) == 2:
+        odszyfruj()
+
+
+print(wynik)
