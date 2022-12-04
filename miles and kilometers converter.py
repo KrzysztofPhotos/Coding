18c5cbbbc59fb7315ecd758d4f6fd5f71ef7b24d
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-04 08:18:39 +0100

    male zmiany

---
+++ miles and kilometers converter.py
@@ -0,0 +1,20 @@
+
+mile = 0.621371192
+
+var = int(input("[Convert\n[Miles to kilometers -> 1\n[Kilometers to miles -> 2\n"))
+
+def mi_to_km():
+    print('chuj')
+
+def km_to_mi():
+    print('chujxd')
+
+if var == 1:
+    # mi to km
+    mi_to_km()
+elif var == 2:
+    # km to mi
+    km_to_mi()
+else:
+    print("Bad value! 1-2")
+
