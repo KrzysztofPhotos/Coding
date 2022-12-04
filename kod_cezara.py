633bccd7756dbd1c4c39f184574aeae94def4272
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-04 15:27:28 +0100

    chyba działający kod z poprawkami

--- kod_cezara.py
+++ kod_cezara.py
@@ -1,6 +1,5 @@
 # SZYFR CEZARA
 
-dlugosc = len(text)
 wynik = []
 alfabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
@@ -17,6 +16,7 @@ def szyfruj():
             if index > 25:
                 index = index - 26
             wynik.append(alfabet[index])
+    rezultat()
 
 def odszyfruj():
     text = str(input("Wpisz text do odszyfrowania: "))
@@ -25,20 +25,28 @@ def odszyfruj():
             wynik.append(" ")
         else:
             index = alfabet.index(i)
-            index = int(index) + 3
-            if index > 25:
-                index = index - 26
+            index = int(index) - 3
             wynik.append(alfabet[index])
+    rezultat()
+
+def rezultat():
+    # konwersja listy do stringa oraz wypisanie go
+    wynik2 = ""
+    for i in wynik:
+
+        wynik2 = wynik2 + str(i)
+
+    print(wynik2)
 
 
-print("Szyfruj -> 1\nOdszyfruj -> 2")
-ask = input("")
+while True:
+    print("Szyfruj -> 1\nOdszyfruj -> 2")
+    ask = input("")
 
-if ask.isdigit():
-    if int(ask) == 1:
-        szyfruj()
-    elif int(ask) == 2:
-        odszyfruj()
-
-
-print(wynik)
+    if ask.isdigit():
+        if int(ask) == 1:
+            szyfruj()
+        elif int(ask) == 2:
+            odszyfruj()
+        else:
+            print('Wpisz tylko 1 lub 2.')
