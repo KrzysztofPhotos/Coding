9c5959846a457bf58d5659104d3a623a72c65bec
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2022-12-04 12:43:14 +0100

    zmiany

---
+++ find_the_largest_among_three_numbers.py
@@ -0,0 +1,14 @@
+# the program asks user to 3 numbers and the program has to organise from smallest to highest
+
+a = float(input("Type first value: "))
+b = float(input("Type second value: "))
+c = float(input("Type third value: "))
+
+if a < b:
+    # b odpada (zostaje tylko a i c)
+    if a > c:
+
+
+
+elif b > a:
+    # a odpada
---
+++ shopping_calculator.py
@@ -0,0 +1,5 @@
+
+# użytkownik wpisuje wartości
+# na samym początku podajemy nasz budżet
+# do wszystkiego jest funkcja (wartość, opis)
+# na koniec wszystko jest sumowane oraz wypisana zostanie cała lista zakupowa wraz z ceną w ładnym wykonaniu
