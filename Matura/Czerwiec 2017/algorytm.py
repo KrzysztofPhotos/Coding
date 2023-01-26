ba0450875e8a72a3aa5f1efecbf3759a6ee18e90
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-01-26 09:41:15 +0100

    save

--- Matura/Czerwiec 2017/algorytm.py
+++ Matura/Czerwiec 2017/algorytm.py
@@ -11,6 +11,9 @@ file = open("punkty.txt",   'r')
 Lines = file.readlines()
 
 obieliczbypierwsze = 0
+krawedz = 0
+wewnatrz = 0
+zewnatrz = 0
 
 count = 0
 for line in Lines:
@@ -21,11 +24,25 @@ for line in Lines:
     x = tablica[0]
     y = tablica[1]
 
+    # 4.1
     if isprime(int(x)) and isprime(int(y)):
         obieliczbypierwsze += 1
 
+    # 4.2
+
+
+    # 4.4
+    if int(x) == -5000 or int(x) == 5000 and -5000 <= int(y) <= 5000:
+        krawedz += 1
+    elif int(y) == -5000 or int(y) == 5000 and -5000 <= int(x) <= 5000:
+        krawedz += 1
+    elif 5000 > int(x) > -5000 and 5000 > int(y) > -5000:
+        wewnatrz += 1
+    else:
+        zewnatrz += 1
 
 print('obie liczby sa pierwsze: ' + str(obieliczbypierwsze))
-
-
+print('na zewnatrz ' + str(zewnatrz))
+print('wewnatrz: ' + str(wewnatrz))
+print('krawedz: ' + str(krawedz))
 
