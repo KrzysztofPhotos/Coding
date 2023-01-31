99c7d583b9a47cbb4f9124370b938f96c94f3b70
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-01-31 18:31:07 +0100

    save

--- Matura/Lipiec 2020/dokumenty.py
+++ Matura/Lipiec 2020/dokumenty.py
@@ -7,6 +7,37 @@ def palindrom(identyfikator):
     else:
         return False
 
+def kontrola(identyfikator):
+    rezultat = 0
+    wartosci = {"A": 10,
+                "B": 11,
+                "C": 12,
+                "D": 13,
+                "E": 14,
+                "F": 15,
+                "G": 16,
+                "H": 17,
+                "I": 18,
+                "J": 19,
+                "K": 20,
+                "L": 21,
+                "M": 22,
+                "N": 23,
+                "O": 24,
+                "P": 25,
+                "Q": 26,
+                "R": 27,
+                "S": 28,
+                "T": 29,
+                "U": 30,
+                "V": 31,
+                "W": 32,
+                "X": 33,
+                "Y": 34,
+                "Z": 35
+                }
+    if identyfikator[0]
+
 
 plik = open("DANE/identyfikator.txt")
 plik = plik.readlines()
@@ -20,8 +51,6 @@ for line in plik:
     elementy.append(element)
     el_strip.append(striped)
 
-#print(elementy)
-#print(el_strip)
 
 wyniki = open('wyniki4_2.txt', 'w')
 for i in elementy:
