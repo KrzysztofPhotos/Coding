9fd366c060b19994b7c5f2485f3c506d1a3b4df6
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-01-31 18:48:11 +0100

    save

--- Matura/Lipiec 2020/dokumenty.py
+++ Matura/Lipiec 2020/dokumenty.py
@@ -7,36 +7,26 @@ def palindrom(identyfikator):
     else:
         return False
 
+
 def kontrola(identyfikator):
     rezultat = 0
-    wartosci = {"A": 10,
-                "B": 11,
-                "C": 12,
-                "D": 13,
-                "E": 14,
-                "F": 15,
-                "G": 16,
-                "H": 17,
-                "I": 18,
-                "J": 19,
-                "K": 20,
-                "L": 21,
-                "M": 22,
-                "N": 23,
-                "O": 24,
-                "P": 25,
-                "Q": 26,
-                "R": 27,
-                "S": 28,
-                "T": 29,
-                "U": 30,
-                "V": 31,
-                "W": 32,
-                "X": 33,
-                "Y": 34,
-                "Z": 35
-                }
-    if identyfikator[0]
+    wartosci = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15, "G": 16, "H": 17, "I": 18, "J": 19, "K": 20,
+                "L": 21, "M": 22, "N": 23, "O": 24, "P": 25, "Q": 26, "R": 27, "S": 28, "T": 29, "U": 30, "V": 31,
+                "W": 32, "X": 33, "Y": 34, "Z": 35}
+    var_1 = identyfikator[0]
+    var_2 = identyfikator[1]
+    var_3 = identyfikator[2]
+    var_1 = wartosci[var_1]
+    var_2 = wartosci[var_2]
+    var_3 = wartosci[var_3]
+    rezultat += int(var_1) * 7 + int(var_2) * 3 + int(var_3) + int(identyfikator[4]) * 7 + int(identyfikator[5]) * 3 + int(identyfikator[6]) + int(identyfikator[7]) * 7 + int(identyfikator[8]) * 3
+
+    liczbadodzielenia = identyfikator[3]
+
+    if rezultat % 10 == liczbadodzielenia:
+        return True
+    else:
+        return False
 
 
 plik = open("DANE/identyfikator.txt")
@@ -51,11 +41,17 @@ for line in plik:
     elementy.append(element)
     el_strip.append(striped)
 
-
-wyniki = open('wyniki4_2.txt', 'w')
+wyniki2 = open('wyniki4_2.txt', 'w')
 for i in elementy:
     if palindrom(i):
         zmienna = str(i)
-        wyniki.write(zmienna)
-        wyniki.write("\n")
+        wyniki2.write(zmienna)
+        wyniki2.write("\n")
 
+wyniki3 = open('wyniki4_3.txt', 'w')
+for i in elementy:
+    print(i)
+    if kontrola(i):
+        zmienna = str(i)
+        wyniki3.write(zmienna)
+        wyniki3.write("\n")
