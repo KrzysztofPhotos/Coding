99346e1533c5a2d3287e221aa31ce8302c7f8d75
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-16 16:48:48 +0200

    działa :D

--- Scratch/matura 2021 maj programowanie/neon.py
+++ Scratch/matura 2021 maj programowanie/neon.py
@@ -1,9 +1,7 @@
 lines = []
-
-with open("instrukcje.txt", encoding='UTF-8') as f:
+with open("przyklad.txt", encoding='UTF-8') as f:
     for line in f:
         lines.append(line.strip())
-
 
 moj_napis = []
 for i in lines:
@@ -19,10 +17,8 @@ for i in lines:
     elif z[0] == "PRZESUN":
 
         zmienna = 0
-
         while moj_napis[zmienna] != z[1]:
             zmienna += 1
-            print(zmienna)
         if ord(z[1])+1 > 90:
             nowa_liczba = chr(ord(z[1]) + 1 - 26)
         else:
@@ -36,8 +32,32 @@ wynik_koncowy = ""
 for i in moj_napis:
     wynik_koncowy += i
 
-s = open('wynik4.txt','w')
-# s.write("4.1 "+ )
-# s.write("\n4.2 "+ )
+dlugosc = len(wynik_koncowy)
+
+#4.2
+licznik = 1
+save = 0
+przewaga = ""
+sam_poczatek_lista = []
+for i in lines:
+    rozdzielone = i.split()
+    sam_poczatek_lista.append(rozdzielone[0])
+
+for i in range(len(sam_poczatek_lista)-1):
+    if sam_poczatek_lista[i] == sam_poczatek_lista[i+1]:
+        # dwa takie same obok siebie
+        licznik += 1
+        if licznik > save:
+            przewaga = sam_poczatek_lista[i]
+            save = licznik
+
+    else:
+        # dwa różne obok siebie zatem zeruj
+        licznik = 1
+
+
+s = open('wynik4.txt', 'w')
+s.write("4.1 " + str(dlugosc))
+s.write("\n4.2 " + str(przewaga) + str(save))
 # s.write("\n4.3 "+ )
 s.write("\n4.4 " + str(wynik_koncowy))
