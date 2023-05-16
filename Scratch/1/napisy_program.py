5adc0c8db4e2afe2d1b44b7f156f3783ad2c7e90
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-16 06:41:01 +0200

    save

--- Scratch/1/napisy_program.py
+++ Scratch/1/napisy_program.py
@@ -4,7 +4,7 @@ lines = []
 with open("przyklad.txt", encoding='UTF-8') as f:
     for line in f:
         lines.append(line.strip())
-    print(lines)
+    #print(lines)
 
 
 numerki = ""
@@ -17,5 +17,6 @@ dlugosc = len(numerki)
 s = open("wynik4.txt", "w")
 s.write("4.1 " + str(dlugosc) + "\n")
 
-
-print(lines)
+zmienna_liczba = 20
+for i in range(50):
+    aktualne = lines[zmienna_liczba]
