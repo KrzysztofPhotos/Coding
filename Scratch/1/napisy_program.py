0cc08dba66f47b9400e585d88e24c2b6d1dd1925
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-16 06:47:07 +0200

    auto save

--- Scratch/1/napisy_program.py
+++ Scratch/1/napisy_program.py
@@ -17,6 +17,15 @@ dlugosc = len(numerki)
 s = open("wynik4.txt", "w")
 s.write("4.1 " + str(dlugosc) + "\n")
 
-zmienna_liczba = 20
+
+odp = ""
+zmienna_liczba = 19
+cyferka = 0
 for i in range(50):
     aktualne = lines[zmienna_liczba]
+    znaczek = aktualne[cyferka]
+    odp += str(znaczek)
+    zmienna_liczba += 20
+    cyferka += 1
+
+s.write("\n4.2 " + str(odp))
