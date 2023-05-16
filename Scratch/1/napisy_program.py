8abac5d9048aa12476a1e916ef6299c41e4c8d65
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-16 06:39:39 +0200

    save

---
+++ Scratch/1/napisy_program.py
@@ -0,0 +1,21 @@
+
+lines = []
+
+with open("przyklad.txt", encoding='UTF-8') as f:
+    for line in f:
+        lines.append(line.strip())
+    print(lines)
+
+
+numerki = ""
+for i in lines:
+    for k in range(len(i)):
+        if i[k].isnumeric():
+            numerki += str(i[k])
+
+dlugosc = len(numerki)
+s = open("wynik4.txt", "w")
+s.write("4.1 " + str(dlugosc) + "\n")
+
+
+print(lines)
