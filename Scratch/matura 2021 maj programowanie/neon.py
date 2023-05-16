2cdb3176580fc52f5e14df97ca0302fae0f409f1
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-16 11:22:26 +0200

    save

--- Scratch/matura 2021 maj programowanie/neon.py
+++ Scratch/matura 2021 maj programowanie/neon.py
@@ -4,3 +4,17 @@ with open("przyklad.txt", encoding='UTF-8') as f:
     for line in f:
         lines.append(line.strip())
 
+
+moj_napis = ""
+for i in lines:
+
+    z = i.split()
+    print(z[0])
+    if z[0] == "DOPISZ":
+        print('dopisz')
+    elif z[0] == "USUN":
+        print('kasuj to')
+    elif z[0] == "ZMIEN":
+        print("zmieniaj mi to")
+    elif z[0] == "PRZESUN":
+        print("przesuwaj to mi!")
