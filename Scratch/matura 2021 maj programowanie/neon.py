5909555c376d5d8b1496c010dde13524969b6817
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-16 16:32:08 +0200

    działa :D

--- Scratch/matura 2021 maj programowanie/neon.py
+++ Scratch/matura 2021 maj programowanie/neon.py
@@ -1,12 +1,13 @@
 lines = []
 
-with open("przyklad.txt", encoding='UTF-8') as f:
+with open("instrukcje.txt", encoding='UTF-8') as f:
     for line in f:
         lines.append(line.strip())
 
+
 moj_napis = []
 for i in lines:
-    #print(i)
+
     z = i.split()
     if z[0] == "DOPISZ":
         moj_napis.append(str(z[1]))
@@ -17,59 +18,26 @@ for i in lines:
         moj_napis.append(str(z[1]))
     elif z[0] == "PRZESUN":
 
-        # szukamy miejsca które jest do zmiany
-        miejsce = len(moj_napis)-1
-        for m in range(len(moj_napis)):
-            od_ostatniego = len(moj_napis)-m-1
+        zmienna = 0
 
-            miejsce -= 1
-            if moj_napis[od_ostatniego] == z[1]:
-                break
-            #miejsce -= 1
-
+        while moj_napis[zmienna] != z[1]:
+            zmienna += 1
+            print(zmienna)
         if ord(z[1])+1 > 90:
             nowa_liczba = chr(ord(z[1]) + 1 - 26)
         else:
             nowa_liczba = chr(ord(z[1])+1)
 
-        moj_napis[miejsce] = nowa_liczba
 
 
-        # index = len(moj_napis)-1
-        #
-        # while str(moj_napis[index]) != str(z[1]):
-        #     index -= 1
-        #
-        # print(index)
-        #
-        # if ord(z[1])+1 > 90:
-        #     nowa_liczba = chr(ord(z[1]) + 1 - 26)
-        # else:
-        #     nowa_liczba = chr(ord(z[1])+1)
-        #
-        # moj_napis[index] = nowa_liczba
+        moj_napis[zmienna] = nowa_liczba
 
-
-#print(moj_napis)
-
-# całkowita długość napisu
-napis_dlugosc = len(moj_napis)
-
-s = open("wynik4.txt", "w")
-
-s.write("4.1 " + str(napis_dlugosc))
-
-napis = ""
+wynik_koncowy = ""
 for i in moj_napis:
+    wynik_koncowy += i
 
-    napis += i
-s.write("\n\n4.4 " + str(napis))
-
-
-count = 0
-przewaga = ""
-
-for i in lines:
-    linia = i.strip()
-
-
+s = open('wynik4.txt','w')
+# s.write("4.1 "+ )
+# s.write("\n4.2 "+ )
+# s.write("\n4.3 "+ )
+s.write("\n4.4 " + str(wynik_koncowy))
