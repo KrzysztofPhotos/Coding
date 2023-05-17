b3c0c7a51c9f3420cca2630dd3b9379f1091ddde
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-17 17:43:45 +0200

    save

--- Matura/Maj 2019/liczby.py
+++ Matura/Maj 2019/liczby.py
@@ -1,6 +1,28 @@
 lines = []
-with open("liczby.txt", encoding='UTF-8') as f:
+with open("przyklad.txt", encoding='UTF-8') as f:
     for line in f:
         lines.append(line.strip())
 
-print(lines)
+
+# print(lines)
+
+
+
+potegi_3 = []
+for i in range(100):
+    number = 3 ** i
+    potegi_3.append(number)
+    # tablice już mamy z potegami 3
+
+for i in range(99):
+    if liczba == potegi_3[i]:
+        print("jest jakas liczba")
+
+
+
+wynik = 0
+for num in lines:
+    if czy_potega(num):
+        wynik += 1
+
+print(wynik)
