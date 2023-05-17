3a18cbadc6cea45dbffd3d450c1a22eaf965b81a
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-17 18:25:24 +0200

    save

--- Matura/Maj 2019/liczby.py
+++ Matura/Maj 2019/liczby.py
@@ -1,3 +1,4 @@
+import math
 lines = []
 with open("liczby.txt", encoding='UTF-8') as f:
     for line in f:
@@ -10,6 +11,19 @@ for liczba in lines:
         if int(liczba) == int(z):
             ile += 1
 
-s = open("wynik4.txt","w")
-s.write("4.1 " + str(ile))
+s = open("wynik4.txt", "w")
+s.write("4.1 " + str(ile) + "\n\n")
 
+s.write("4.2")
+for element in lines:
+    # rozbijamy na kawałki
+    temp = []
+    for cyfra in element:
+        temp.append(cyfra)
+    #print(temp)
+    wynik = 0
+    for k in temp:
+        wynik += math.factorial(int(k))
+    #print(wynik)
+    if int(element) == int(wynik):
+        s.write("\n" + str(element))
