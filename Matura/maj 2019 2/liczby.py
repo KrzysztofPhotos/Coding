f6014f2edad86643c14fd18befe3937b0292fc80
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-18 18:16:48 +0200

    save

--- Matura/maj 2019 2/liczby.py
+++ Matura/maj 2019 2/liczby.py
@@ -1,17 +1,13 @@
 import math
 
 lines = []
-with open("przyklad.txt",encoding='UTF-8') as f:
+with open("liczby.txt",encoding='UTF-8') as f:
     for line in f:
         lines.append(line.strip())
-
-print(lines)
 
 potegi = []
 for i in range(11):
     potegi.append(3**i)
-
-print(potegi)
 
 h = 0
 for i in lines:
@@ -24,19 +20,17 @@ s = open("wyniki4.txt",'w')
 s.write("4.1 " + str(h))
 
 # 4.2
-zmienna = 0
+takie = []
 # rozbijam liczbe na pojedyncze cyfry
 for i in lines:
     wynik = 0
     for znak in i:
-        #print(znak)
         wynik += math.factorial(int(znak))
-    #print(wynik)
-    #print(i)
     if int(i) == int(wynik):
-        zmienna += 1
+        takie.append(i)
 
-s.write("4.2 " + str(zmienna))
-
+s.write("\n\n4.2")
+for i in takie:
+    s.write(str(i) + "\n")
 
 
