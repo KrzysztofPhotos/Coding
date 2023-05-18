8a2da74b71a1f0928516c9a4a9b6327912ad6b22
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-18 18:08:59 +0200

    save

--- Matura/maj 2019 2/liczby.py
+++ Matura/maj 2019 2/liczby.py
@@ -10,3 +10,16 @@ for i in range(11):
     potegi.append(3**i)
 
 print(potegi)
+
+h = 0
+for i in lines:
+    # patrzymy na jedna linijek
+    for n in range(11):
+        if int(i) == int(potegi[n]):
+            h += 1
+
+s = open("wyniki4.txt",'w')
+s.write("4.1 " + str(h))
+
+
+
