b4636e0a7586e73b03cedbf50f7aa6b731082940
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-21 20:19:00 +0200

    save

--- Learning/2/s.py
+++ Learning/2/s.py
@@ -1,6 +1,12 @@
 tablica = [0,6,2,3,1,1,2,3,2,2,1,1,1,2,2,3,4,4,5,6,7,8,3,2,1,2,3,3,2,3,2,1,3]
-
+tablica3 = [10,11,12,13,14,15,16,18,69]
 # TODO posortuj te tablice oraz usun duplikaty
-# TODO list -> dict.fromkeys
+# TODO list -> set
 
+# tablica = list(set(tablica+tablica3))
+# tablica.sort()
+# print(tablica)
 
+# tablica = [1,2,3,2,1,2,2,3,1]
+# tablica = list(set(tablica))
+# print(tablica)
