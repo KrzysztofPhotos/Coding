6ec29a0726dfb45d2af11d2c8018391187ae1082
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-21 15:13:33 +0200

    save

--- Matura/Czerwiec 2017_2/punkty.py
+++ Matura/Czerwiec 2017_2/punkty.py
@@ -4,7 +4,6 @@ with open('punkty.txt', encoding='UTF-8') as f:
     for line in f:
         lines.append(line.strip())
 
-
 def cyfropodobne(x,y):
     tablica_x = []
     tablica_y = []
@@ -12,25 +11,30 @@ def cyfropodobne(x,y):
         tablica_x.append(x[i])
     tablica_x.sort()
     tablica_x = list(dict.fromkeys(tablica_x))
-    print(tablica_x)
 
     for i in range(len(str(y))):
         tablica_y.append(y[i])
     tablica_y.sort()
     tablica_y = list(dict.fromkeys(tablica_y))
-    print(tablica_y)
 
     if tablica_x == tablica_y:
-        print("true")
+        return True
+    else:
+        return False
+
 
 cyfropodobne("22113377","32172")
 
 licznik = 0
+licznik_cyfropodobne = 0
 for i in lines:
     rozdzielone = i.split(" ")
     if sympy.isprime(int(rozdzielone[0])) and sympy.isprime(int(rozdzielone[1])):
         licznik += 1
 
+    if cyfropodobne(rozdzielone[0],rozdzielone[1]):
+        licznik_cyfropodobne += 1
 
 s = open('wyniki4.txt','w')
 s.write("4.1 " + str(licznik))
+s.write("\n4.2 " + str(licznik_cyfropodobne))
