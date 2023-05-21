38d786ddaa7d50e46faed750dc3843d509b89af0
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-21 10:28:13 +0200

    save

--- Learning/2/script.py
+++ Learning/2/script.py
@@ -9,3 +9,29 @@ for i in lines:
         slowa[i] = len(i)
 
 print(slowa)
+
+# numerami = {}
+#
+# for slowo in lines:
+#     if len(slowo) >= 19:
+#         numerami[len(slowo)] = slowo
+#
+# print(numerami)
+
+tabelka = ['he', 'puu']
+slowniczek = {}
+
+slowniczek['xD'] = tabelka
+
+print(slowniczek)
+
+oceny = [2,4,5,2,5,2,3,4,2]
+
+for i in range(len(oceny)):
+    print(oceny[i], end=" ")
+
+for ocena in oceny:
+    print(ocena, end=" ")
+
+for i, ocena in enumerate(oceny):
+    print(i, ocena)
