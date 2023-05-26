1a37c418c554ee8938ced05911e5774215962378
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2023-05-26 06:57:22 +0200

    save

--- Learning/2/s.py
+++ Learning/2/s.py
@@ -1,7 +1,8 @@
+import sympy
+
 tablica = [0,6,2,3,1,1,2,3,2,2,1,1,1,2,2,3,4,4,5,6,7,8,3,2,1,2,3,3,2,3,2,1,3]
 tablica3 = [10,11,12,13,14,15,16,18,69]
 # TODO posortuj te tablice oraz usun duplikaty
 # złącz te tablice
-
 
 
