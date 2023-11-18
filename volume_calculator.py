89ebd11191ba9d802aa9fac9b03cb02801dd8965
unknown <krzysztofkwiatkowski555@gmail.com>
2023-11-18 13:23:02 +0100

    new idea

---
+++ volume_calculator.py
@@ -0,0 +1,17 @@
+# program to calculate the volume/area of sphere, cube or pyramid etc.
+
+print("Welcome to volume and area calculator!\nPlease select which operation do you wanna do:")
+operation = input("Calculate\n1) Area\n2) Volume")
+
+if operation == 1:
+    
+elif operation == 2:
+    
+else:
+    print("Error! Incorrect value!")
+
+def volume_calculator():
+    print("Calculate the volume of\n1) Sphere\n2) Cube\n3) Pyramid\n4) Cuboid")
+
+def area_calculator():
+    print("\nCalculate the area of\n1) Square\n2) Cube\n3) Pyramid\n4) Cuboid")
