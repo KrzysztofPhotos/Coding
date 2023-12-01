48630d1301442059105032ac485a971de7e499dd
Krzychu <krzysztofkwiatkowski555@gmail.com>
2023-12-01 16:27:06 +0100

    save

---
+++ LeetCode/1. Two Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        for i in range(len(nums)-1):
+
+            if nums[i] + nums[i+1] == target:
+                return [i, i+1]
+                #print("[" + str(i) + "," + str(i+1) + "]")  
