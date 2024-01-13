2e6c179fe05935f1657bcec19d2d9f98a63209c4
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2024-01-13 10:37:25 +0100

    I have no idea

--- hangman.py
+++ hangman.py
@@ -7,21 +7,34 @@ word_list = ['police', 'buzzard', 'lucky', 'jackpot', 'wall', 'stronghold', 'ice
 # - etc
 
 import random
+import time
 
 
 
 word_to_guess_str = word_list[random.randint(0, len(word_list)-1)]
 
-word = []
+hearts = 5
 
+word = []
 for i in word_to_guess_str:
     word.append(i)
 
-print(word)
+print(word) # DEBUG TOOL
 
+print("_ " * len(word_to_guess_str))
 
+while hearts > 0:
 
+    time.sleep(1)
+    letter_input = input("Guess a letter: ")
 
+    for i in range(len(word)):
+        if letter_input == word[i]:
+            print("success")
 
-#letter_input = input("Guess a letter: ")
+            #now we need to print _ _ _ _ _ _ with lettters
+            # for example _ a _ a _ _ and save it to variable
+        else:
+            print("wrong letter" + " HEARTS: " + str(hearts))
+            hearts -= 1
 
