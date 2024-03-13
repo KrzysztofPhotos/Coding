269834b1d7facb3439acd49bba45f798a626603a
Krzysztof Kwiatkowski <krzysztofkwiatkowski555@gmail.com>
2024-03-13 11:40:19 +0100

    save

--- OOP.py
+++ OOP.py
@@ -12,3 +12,40 @@ x.conjugate()
 print(x.i) # 1.0
 
 
+
+
+class Customer:
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        
+    def __str__(self):
+        return "Klient [imię: " + self.first_name + ", nazwisko: " + self.last_name + ", email: " + self.email + "]"
+    
+    def short_name(self):
+        return self.first_name[0] + self.last_name[0]
+    
+customer1 = Customer("Jan","Nowak","jan.nowak@gmail.com")
+customer2 = Customer("Anna","Kowalska","anna.kowalska@gmail.com")
+
+print(customer1)
+print(customer2)
+
+print(customer1.short_name())
+
+
+class Company:
+    def __init__(self, company_name, nip, mail):
+        self.company_name = company_name
+        self.nip = nip
+        self.mail = mail
+        
+    def print_label(self):
+        print('Company name: ' + self.company_name + "; NIP: " + self.nip)
+        
+company1 = Company("Cisco", "502876485", "contact@cisco.com")
+
+company1.print_label()
+
+
